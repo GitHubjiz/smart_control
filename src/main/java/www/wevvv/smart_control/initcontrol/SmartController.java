899b90b41d2a8036b991d94d6774ee3b79760aad
GitHubjiz <1174250709@qq.com>
2020-07-03 13:48:56 +0800

    紫气东来

---
+++ src/main/java/www/wevvv/smart_control/initcontrol/SmartController.java
@@ -0,0 +1,14 @@
+package www.wevvv.smart_control.initcontrol;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SmartController {
+
+    /*@GetMapping("/index")
+    public String index (){
+        return "index";
+    }*/
+
+}
---
+++ src/test/java/www/wevvv/smart_control/SmartControlApplicationTests.java
@@ -0,0 +1,13 @@
+package www.wevvv.smart_control;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartControlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
